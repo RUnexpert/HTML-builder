c32ebb5ea9697f226b003aa097076a411d963153
RUnexpert <aldali911@gmail.com>
2021-11-09 16:06:30 +0300

    fix: fix 04 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,12 +1,17 @@
 const fs = require("fs").promises;
+const FS = require("fs");
 const path = require("path");
 const dir = path.join(__dirname, "files");
+const copyDir = path.join(__dirname, "files-copy/");
 
 async function copyFiles() {
-  const directory = "./files-copy";
-  fs.rmdir(directory, { recursive: true, force: true }).then(() =>
-    console.log("Directory updated")
-  );
+  FS.readdir(copyDir, (err, files) => {
+    files.forEach((file) => {
+      FS.unlink(copyDir + file, function (err) {
+        if (err) return console.log(err);
+      });
+    });
+  });
 
   await fs.mkdir(path.join(__dirname, "files-copy"), { recursive: true });
   const files = await fs.readdir(dir);
